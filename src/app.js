f6410e2a0849b876a4d5def014d07561c567824b
xfontainex <maralafontaine@gmail.com>
2022-12-08 07:03:17 +0000

    improved the CSS

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   let particularExcuse = " You will not believe me but... <br> ";
 
-  let who = ["Chuck Norris ", "Dolly Parton ", "Pee Wee Herman ", "Cardi B "];
-  let action = ["ate ", "threw away ", "threw up on ", "spilled egg nog "];
+  let who = ["Chuck Norris ", "Dolly Parton ", "Pee Wee Herman ", "Cardi-B "];
+  let action = ["ate ", "threw away ", "threw up on ", "spilled egg nog on "];
   let what = ["my homework ", "my code ", "my assignment ", "my laptop "];
   let when = ["before class!", "during the duck race!", "just now!", "today!"];
 
