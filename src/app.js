74c0d0ef0b2205340b8df8d124f37a085dd0fb19
xfontainex <maralafontaine@gmail.com>
2022-12-08 06:59:05 +0000

    finished the assignment

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let particularExcuse = " You will not believe me but... <br> ";
+
+  let who = ["Chuck Norris ", "Dolly Parton ", "Pee Wee Herman ", "Cardi B "];
+  let action = ["ate ", "threw away ", "threw up on ", "spilled egg nog "];
+  let what = ["my homework ", "my code ", "my assignment ", "my laptop "];
+  let when = ["before class!", "during the duck race!", "just now!", "today!"];
+
+  // who
+  function whoGenerator() {
+    let i = Math.floor(Math.random() * who.length);
+    particularExcuse += who[i];
+  }
+
+  // action
+  function actionGenerator() {
+    let i = Math.floor(Math.random() * action.length);
+    particularExcuse += action[i];
+  }
+
+  // what
+  function whatGenerator() {
+    let i = Math.floor(Math.random() * what.length);
+    particularExcuse += what[i];
+  }
+
+  // when
+  function whenGenerator() {
+    let i = Math.floor(Math.random() * when.length);
+    particularExcuse += when[i];
+  }
+
+  whoGenerator();
+  actionGenerator();
+  whatGenerator();
+  whenGenerator();
+
+  document.getElementById("excuse").innerHTML = particularExcuse;
 };
